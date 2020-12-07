cf7b0ed0f3fb48b9eb730c5db5cf342e53a22eb2
cramerm <mecramer@yahoo.com>
2020-12-07 14:20:50 -0500

    Added query to get pizza info from Sanity using graphql, added map function to display them on the page.

--- master-gatsby/src/pages/pizzas.js
+++ master-gatsby/src/pages/pizzas.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import PizzaList from '../components/PizzaList'
 
-function PizzasPage() {
-  return <p>Hey! I'm the pizzas page.</p>
+function PizzasPage({ data }) {
+  console.log(data.pizzas)
+  const pizzas = data.pizzas.nodes
+  return (
+    <>
+      <PizzaList pizzas={pizzas} />
+    </>
+  )
 }
 
 export default PizzasPage
+
+export const query = graphql`
+  query PizzaQeury {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
